10d356a5174596bf25b8e2201bb41044400a4af0
Jason Ferguson <fergusonjason@gmail.com>
2020-10-12 05:41:15 -0400

    start breaking the lightkurve functions into separate functions. Add a bunch of todos so people looking at this don't think I'm a moron

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-#from flask import Flask, jsonify, request
+#
+# API Interface to the lighkurve functions
+#
+# TODO: refactor this into a decent-looking webapp
+# TODO: use path variables to determine which catalog to use
+# TODO: validate contents of request body
+
 from quart import Quart, jsonify, request, send_file
 import platform
 from lightkurve import search_targetpixelfile
@@ -18,8 +24,6 @@ async def query_mast():
 @app.route("/catalog/getEntry", methods = ["POST"])
 async def get_catalog_entry():
 
-    print(f"content_type: {request.content_type}. content encoding: {request.content_encoding}, mimetype: {request.mimetype}")
-
     request_json = await request.get_json()
     target = request_json["target"]
     catalog = request_json["catalog"]
@@ -34,7 +38,6 @@ async def get_pixelfile():
 
     request_json = await request.get_json()
 
-    print(f"request json: {request_json}")
     target = request_json.get("target")
     if target == None:
         return {"ERROR":"No target specified"}
@@ -55,20 +58,63 @@ async def get_pixelfile():
         tpf = search_targetpixelfile(target=target, mission="Kepler", quarter=quarter).download()
         pass
 
+    # TODO: be restful and return a 404 here
     if tpf == None:
         return {"ERROR":"No results from search_targetpixelfile"}
 
-
-    # tpf = search_targetpixelfile(target=target, quarter=quarter, mission=mission).download()
 
     tpf_plot = tpf.plot()
     tpf_plot.get_figure().savefig("test.png")
     return await send_file("test.png", mimetype="application/png", as_attachment=True)
 
+@app.route("/lightkurve")
+async def get_lightkurve():
 
-def tess_search(target, sector):
+    request_json = await request.get_json()
+    mission = request_json.get("mission")
+    target = request_json.get("target")
 
-    result = search_targetpixelfile(target=target, sector=sector, mission="TESS")
-    return result
+    tpf = None
+    if mission == "TESS":
+        sector = request_json.get("sector")
+        tpf = search_targetpixelfile(target=target, mission="TESS", sector=sector).download()
+        pass
+
+    if tpf == None:
+        # TODO: be restful and return a 404 here
+        return {}
+
+    lightkurve_options = request_json.get("options")
+    if lightkurve_options == None:
+        lightkurve_options = {}
+
+    result = tpf.to_lightkurve(aperture_mask="ALL")
+    if (lightkurve_options.get("flatten") == True):
+        result = result.flatten(window_length=401)
+
+    result_plot = result.plot()
+    result_plot.figure().savefig("test.png")
+
+    return await send_file("test.png", mimetype="application/png", as_attachment=True)
+
+# TODO: options is a bad idea, use **kwargs with default of None or whatever lk expects as default
+# TODO: find out what other options for to_lightkurve are to give the option of using them
+def get_lightkurve(pixelfile, options):
+
+    aperture_mask = options.get("aperture_mask")
+
+    return pixelfile.to_lightkurve(aperture_mask = aperture_mask)
+
+# TODO: options is a bad idea, use **kwargs with default of None or whatever lk expects as default
+# TODO: find out the other options of flatten to give the option of using them
+def flatten_lightkurve(lightkurve, options):
+
+    window_length = options.get("window_length")
+
+    return lightkurve.flatten(window_length=window_length)
+
+def fold_lightkurve(lightkurve, period):
+
+    return lightkurve.fold(period = period)
 
 app.run()
